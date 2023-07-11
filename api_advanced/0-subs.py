785dfc2e8127e43f25a91bc182699bc7a66b513d
mulimuoki001 <b.muoki@alustudent.com>
2023-07-11 18:31:25 +0200

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,26 +1,41 @@
 #!/usr/bin/python3
 """Python script that returns the number of subscribers of a given subreddit"""
 
-
 import requests
 
-# Get the number of subscribers of a subreddit
+
 def number_of_subscribers(subreddit):
-    api_url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    #used to set the user agent header
-    headers = {'User-Agent': 'My Bot/0.1'}
-    #send a GET request to the reddit api
+    """
+    Get the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers.
+
+    """
+    # Create the API URL by formatting the subreddit
+    api_url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    # Set the user agent header
+    headers = {"User-Agent": "My Bot/0.1"}
+
+    # Send a GET request to the API
     response = requests.get(api_url, headers=headers)
-    #Used to check if the request was successful
+
+    # Check if the response was successful
     if response.status_code == 200:
-        #parse the response to json
-        data1 = response.json()
-        #Get the number of subscribers
-        subscribers = data1['data']['subscribers']
-        # used to return the number of subscribers
+        # Parse the response JSON
+        data = response.json()
+
+        # Get the number of subscribers from the data
+        subscribers = data["data"]["subscribers"]
+
+        # Return the number of subscribers
         return subscribers
     else:
-        #return 0 if the request was not successful
+        # If the response was not successful, return 0
         return 0
 
 
