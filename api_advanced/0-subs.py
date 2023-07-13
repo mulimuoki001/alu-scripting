922740b9c2cb6342dead1ef18de45501da30ed8e
mulimuoki001 <b.muoki@alustudent.com>
2023-07-13 20:45:06 +0200

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -6,8 +6,8 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "My User Agent1.0"}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "My User Agent 1.0"}
     response = requests.get(url, headers=headers)
 
     if response.status_code != 200:
