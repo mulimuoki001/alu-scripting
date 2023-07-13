7890308d10cf418784c5e39418a53aaf8e7e69e5
mulimuoki001 <b.muoki@alustudent.com>
2023-07-13 21:00:48 +0200

    1-top_ten.py

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This function retrieves the top ten posts of a given subreddit"""
+
+import requests
+
+
+def top_ten(subreddit):
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {"User-Agent": "My User Agent 1.0"}
+    feedback = requests.get(url, headers=headers)
+
+    if feedback.status_code != 200:
+        print(None)
+    else:
+        data = feedback.json().get("data").get("children")
+        for post in data:
+            print(post.get("data").get("title"))
