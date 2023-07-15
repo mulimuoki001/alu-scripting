308b4650cb7887eed6e13faa0f7887fab45c9685
mulimuoki001 <b.muoki@alustudent.com>
2023-07-15 21:44:31 +0200

    2-recurse.py

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
-"""Recurse"""
+""""Doc"""
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
-    """Returns list with titles of all hot articles in a subreddit """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'after': after}
-    response = requests.get(
-                                url,
-                                headers=headers,
-                                params=params,
-                                allow_redirects=False
-                            )
+def recurse(subreddit, hot_list=[], after=""):
+    """"Doc
+    Reddit sends an after property in the response.
+    Keep retrieving comments until after is null.
+    """
+    url = "https://www.reddit.com/r/{}/hot.json" \
+        .format(subreddit)
+    header = {'User-Agent': 'Mozilla/5.0'}
+    param = {'after': after}
+    res = requests.get(url, headers=header, params=param)
 
-    if response.status_code == 200:
-        data = response.json().get('data')
-        children = data.get('children') if data else None
-        if children:
-            for child in children:
-                hot_list.append(child.get('data').get('title'))
-            after = data.get('after')
-            if after:
-                return recurse(subreddit, hot_list, after)
-            else:
-                return hot_list
-        else:
-            return hot_list
-    else:
+    if res.status_code != 200:
         return None
+    else:
+        json_res = res.json()
+        # print(json_res.get('data').get('after'))
+        after = json_res.get('data').get('after')
+        has_next = \
+            json_res.get('data').get('after') is not None
+        # print(has_next)
+        hot_articles = json_res.get('data').get('children')
+        [hot_list.append(article.get('data').get('title'))
+         for article in hot_articles]
+        # print(len(hot_list))
+        # print(hot_list)
+        return recurse(subreddit, hot_list, after=after) \
+            if has_next else hot_list
